02a22aedd2c0b724bf86e4f70ca7c8717f8d6201
acer <acer@user>
2017-10-13 10:37:43 -0300

    :32

---
+++ Practica Felipe/src/Metodos.java
@@ -0,0 +1,22 @@
+
+public class Metodos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	sum();
+	
+		
+	}
+
+	public static void sum() {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		a=15;
+		b=32;
+		c=a+b;
+		System.out.println("el resultado es: "+c);
+	}
+	
+	}
+
+
